0432e6adfb579e2a91557197092ec7dbe89c1ea5
Igor Katson <igor.katson@gmail.com>
2025-07-02 22:04:12 +0100

    Add binding to device to all sockets except multicast

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 #[cfg(test)]
 mod tests;
 
+mod bind_device;
+mod connect;
 mod error;
 mod multicast;
 mod traits;
@@ -13,6 +15,8 @@ pub mod socket;
 
 pub type TcpListener = MaybeDualstackSocket<tokio::net::TcpListener>;
 pub type UdpSocket = MaybeDualstackSocket<tokio::net::UdpSocket>;
+pub use bind_device::BindDevice;
+pub use connect::{ConnectOpts, tcp_connect};
 pub use multicast::{MulticastOpts, MulticastUdpSocket};
 pub use socket::BindOpts;
 pub use traits::PollSendToVectored;
